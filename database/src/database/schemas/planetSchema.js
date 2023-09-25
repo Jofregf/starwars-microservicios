369bb8b8bcceaa2f360323f330f194aef7c8559d
Guillermo Jofre <jofregf@gmail.com>
2023-09-25 12:58:16 -0300

    feat: :sparkles: Agregado de validaciones y rutas al schema Planet
    Agregamos validaciones al schema y adicionamos las funciones de remover, y actualizar

--- database/src/database/schemas/planetSchema.js
+++ database/src/database/schemas/planetSchema.js
@@ -1,11 +1,37 @@
 const { Schema } = require("mongoose");
+const { ClientError } = require("../../utils/errors");
+const {v4: uuidv4} = require("uuid");
 
 const planetSchema = new Schema({
-    "_id": String,
-    "name": String,
-    "rotation_period": String,
-    "orbital_period": String,
-    "diameter": String,
+    "_id": {
+        type: String, 
+        default: uuidv4
+    },
+    "name": {
+        type: String, 
+        required: true
+    },
+    "rotation_period": {
+        type: String,
+        validate: {
+            validator: (value) => !isNaN(value),
+            message: "El height debe ser un número válido",
+        },
+    },
+    "orbital_period": {
+        type: String,
+        validate: {
+            validator: (value) => !isNaN(value),
+            message: "El height debe ser un número válido",
+        },
+    },
+    "diameter": {
+        type: String,
+        validate: {
+            validator: (value) => !isNaN(value),
+            message: "El height debe ser un número válido",
+        },
+    },
     "climate": String,
     "gravity": String,
     "terrain": String,
@@ -29,5 +55,24 @@ planetSchema.statics.get = async function (id){
 planetSchema.statics.insert = async function (planet){
     return await this.create(planet);
 }
+
+planetSchema.statics.remov = async function (_id) {
+    const result = await this.findByIdAndDelete(_id);
+    if (!result) {
+        throw new ClientError("Planet no encontrado", 404);
+    }
+    return result;
+};
+
+planetSchema.statics.update = async function (_id, planet) {
+    const result = await this.findByIdAndUpdate(_id, planet, {
+        runValidators: true,
+        new: true
+    });
+    if (!result) {
+        throw new ClientError("Planet no encontrado", 404);
+    }
+    return result;
+};
 
 module.exports = planetSchema;
